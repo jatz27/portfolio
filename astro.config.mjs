e86dbc454993a30663d33e1d5c023cf04d18120e
JulioAtz <jatz93@outlook.com>
2025-01-10 23:58:07 -0600

    config deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://jatz27.github.io/portfolio/',
+  base: '/portfolio/',
+});
+
+
+
